734ff4b848faeaa7d99e881a2c5e450be85db1b0
Juan Carvajal <juan.instructor@outlook.com>
2023-04-09 11:42:45 -0500

    fix: rewrite class discussion 1

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // CLASS_LESSON: Objects
 //
-// CLASS_DISCUSSION 1: Write a JavaScript function named 'listObjectProperties' to list the properties of 
+// CLASS_DISCUSSION 1: Write a JavaScript function named 'listObjectProperties' to list the properties of
+// the student object defined below
 
-//console.log(listObjectProperties(student)) => Student values are: David Rayy, VI and 12
+// console.log(listObjectProperties(student)) => Student values are: David Rayy, VI and 12
 
 const student = {
     name: "David Rayy",
     sclass: "VI",
-    rollno: 12
+    rollno: 12,
 };
